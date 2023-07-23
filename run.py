37eac80fb5bd1c68fbc746ca34ee1c1a42409acd
Bowlesy666 <>
2023-07-23 21:55:09 +0000

    Add board and create_board

--- run.py
+++ run.py
@@ -8,13 +8,31 @@ computer_random_move
 
 """
 
+
+board = [' ' for i in range(9)]
+
+
+def create_board():
+    print('   |   |   ')
+    print(' ' + board[0] + ' | ' + board[1] + ' | ' + board[2])
+    print('   |   |   ')
+    print('-----------')
+    print('   |   |   ')
+    print(' ' + board[0] + ' | ' + board[1] + ' | ' + board[2])
+    print('   |   |   ')
+    print('-----------')
+    print('   |   |   ')
+    print(' ' + board[0] + ' | ' + board[1] + ' | ' + board[2])
+    print('   |   |   ')
+
+
 def run_game():
     """
     Runs the game tic-tac-toe displaying the board,
     receiving input from player and taking the computers turns
 
     """
-    print('Its alive! iiiiits aaaaliiiive!')
+    create_board()
 
 
 run_game()
