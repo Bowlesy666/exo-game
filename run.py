f82eaacfcf849df125938289863ed9b425bb2b99
Bowlesy666 <>
2023-07-23 21:44:40 +0000

    Initial commit, run_game

--- run.py
+++ run.py
@@ -1,3 +1,20 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+run_game
+create_board
+validate_input
+check_if_empty
+check_for_winner
+computer_random_move
+
+"""
+
+def run_game():
+    """
+    Runs the game tic-tac-toe displaying the board,
+    receiving input from player and taking the computers turns
+
+    """
+    print('Its alive! iiiiits aaaaliiiive!')
+
+
+run_game()
