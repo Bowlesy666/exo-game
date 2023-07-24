7247450e0a79104e540c679c6de6a4dc14d30c95
Bowlesy666 <>
2023-07-24 21:56:06 +0000

    Update variable + function names

--- run.py
+++ run.py
@@ -1,19 +1,13 @@
 """
-run_game
-done - create_board - done
-done - validate_input
-done - update_board
-done - check_if_empty
-done - check_for_winner -
-done - check_game_over > we have a winner
-done -computer_random_move
-almost done - play_again - has bug, wont accept capitals!
-for loop - score keeping
+colorama
+validation for name input
+txt files
 """
 import random
 
 
-board = [' ' for i in range(9)]
+gameboard_locations_list = [' ' for i in range(9)]
+location = gameboard_locations_list
 winning_combos = [
     (0, 1, 2), (3, 4, 5), (6, 7, 8),  # Winning rows
     (0, 3, 6), (1, 4, 7), (2, 5, 8),  # Winning columns
@@ -25,22 +19,22 @@ computer_score = 0
 player_score = 0
 
 
-def create_board():
+def display_gameboard():
     """
     Creates board to be displayed,
     each section will be numbered to help player
     selection area in centre of each section
     """
     print('1  |2  |3  ')
-    print(' ' + board[0] + ' | ' + board[1] + ' | ' + board[2])
+    print(' ' + location[0] + ' | ' + location[1] + ' | ' + location[2])
     print('   |   |   ')
     print('-----------')
     print('4  |5  |6  ')
-    print(' ' + board[3] + ' | ' + board[4] + ' | ' + board[5])
+    print(' ' + location[3] + ' | ' + location[4] + ' | ' + location[5])
     print('   |   |   ')
     print('-----------')
     print('7  |8  |9  ')
-    print(' ' + board[6] + ' | ' + board[7] + ' | ' + board[8])
+    print(' ' + location[6] + ' | ' + location[7] + ' | ' + location[8])
     print('   |   |   ')
 
 
@@ -49,7 +43,8 @@ def score_display(player_name):
     print(f'    Computer   |     {player_name}')
     print('-------------------------------')
     print('               |')
-    print('       ' + str(computer_score) + '       |       ' + str(player_score) + '\n')
+    middle = '       |       '
+    print('       ' + str(computer_score) + middle + str(player_score) + '\n')
 
 
 def get_message(file):
@@ -64,48 +59,49 @@ def get_message(file):
     return file_data
 
 
-def update_board(chosen_position, player_x_or_o):
+def update_gameboard(chosen_position, player_x_or_o):
     """
     Updates chosen position with either X or O
     """
-    board[chosen_position - 1] = player_x_or_o
+    location[chosen_position - 1] = player_x_or_o
 
 
-def reset_game_board():
-    global board
-    board = [' ' for i in range(9)]
+def reset_gameboard():
+    global location
+    location = [' ' for i in range(9)]
 
 
 def computer_random_choice():
     while True:
         random_position = random.randint(1, 9)
-        if board[random_position - 1] == ' ':
-            update_board(random_position, player_o)
+        if location[random_position - 1] == ' ':
+            update_gameboard(random_position, player_o)
             print(f"Your opponent chose {random_position}")
             break
 
 
-def score_update(player):
+def score_update(player_identity):
     global player_score
     global computer_score
-    if player == 'X':
+    if player_identity == 'X':
         player_score += 1
     else:
         computer_score += 1
 
-def winner_display(player):
+
+def winner_display(player_identity):
     """
     Checks who the player is that won,
     calls the correct message
     to console, calls next game and
     updates score board
     """
-    if player == 'X':
+    if player_identity == 'X':
         message_file = 'winner.txt'
     else:
         message_file = 'loser.txt'
     print(get_message(message_file))
-    create_board()
+    display_gameboard()
 
 
 def play_again(player_name):
@@ -115,17 +111,18 @@ def play_again(player_name):
     """
     score_display(player_name)
     while True:
-        play_again_input = input("Do you want to play again?: (y/n)")
+        play_again_input = input(
+            f"{player_name}, do you want to play again?: (y/n)")
         print(play_again_input.lower())
         if play_again_input.lower() not in ('y', 'n'):
             print('Invalid value, please use lowercase y or n.....')
             continue
 
         if play_again_input == 'y':
-            reset_game_board()
+            reset_gameboard()
             return True
         elif play_again_input == 'n':
-            print('They said no dude!')
+            print('\n Thank you for playing EXO!')
             return False
 
 
@@ -136,24 +133,23 @@ def check_for_winner():
     print('Checking for winner...')
     for combo in winning_combos:
         zero, one, two = combo
-        if board[zero] == board[one] == board[two] != ' ':
-            print("We have a weener!")
+        if location[zero] == location[one] == location[two] != ' ':
             return True
 
 
-def check_game_over(player):
+def check_game_over(player_name):
     """
     Checks if game over - if winner announced or
     if board is full
     """
     print('checking for game over...')
     if check_for_winner():
-        score_update(player)
-        winner_display(player)
+        score_update(player_name)
+        winner_display(player_name)
         return True
-    elif ' ' not in board:
+    elif ' ' not in location:
         print(get_message('tie.txt'))
-        create_board()
+        display_gameboard()
         return True
     else:
         return False
@@ -167,17 +163,17 @@ def run_game(player_name):
     """
     while True:
         print(f"\n\n{player_name} you're up!")
-        create_board()
-        player_input = int(input('Choose an empty space (1-9)'))
+        display_gameboard()
+        player_location_input = int(input('Choose an empty space (1-9)'))
         try:
-            if int(player_input) < 1:
+            if int(player_location_input) < 1:
                 print('number is too little!')
-            elif int(player_input) > 9:
+            elif int(player_location_input) > 9:
                 print('number is too big!')
-            elif board[player_input - 1] != ' ':
+            elif gameboard_locations_list[player_location_input - 1] != ' ':
                 print('Position is already occupied! Try again...')
             else:
-                update_board(int(player_input), player_x)
+                update_gameboard(int(player_location_input), player_x)
                 if check_game_over(player_x):
                     if play_again(player_name):
                         continue
@@ -200,10 +196,9 @@ def main():
     asks for user name
     then runs game and passes player name
     """
-    welcome_message = get_message('welcome.txt')
-    print(welcome_message)
-    player = input('Hey Player! What is your name?: ')
-    run_game(player)
+    print(get_message('welcome.txt'))
+    player_name = input('Hey Player! What is your name?: ')
+    run_game(player_name.capitalize())
 
 
 main()
