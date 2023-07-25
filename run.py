574f137e14c015e9caec4f368af8bf4d33a4c941
Bowlesy666 <>
2023-07-25 13:01:47 +0000

    Test remove console_messages.py, heroku issue

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import random
-from console_messages import welcome_message, winner_message
-from console_messages import loser_message, tie_message
+#from console_messages import welcome_message, winner_message
+#from console_messages import loser_message, tie_message
 from colorama import Fore, Back, Style, init
 init(autoreset=True)
 
@@ -53,6 +53,48 @@ def score_display(player_name):
         Fore.CYAN + '       ' + str(computer_score) +
         middle + Fore.GREEN + str(player_score) + '\n' + Fore.RESET
         )
+
+
+def welcome_message():
+    """
+    Prints the welcome message to
+    the console with colours added
+    from colorama
+    """
+    print(Fore.RED + f'\n    Welcome to EXO!\n')
+    print(Fore.BLUE + " The game of X's and O's\n    Also known as...\n")
+    print('       ' + Fore.YELLOW + Back.BLUE + ' T | I | C ' + Back.RESET)
+    print('       ' + Fore.YELLOW + Back.BLUE + '-----------' + Back.RESET)
+    print('       ' + Fore.YELLOW + Back.BLUE + ' T | A | C ' + Back.RESET)
+    print('       ' + Fore.YELLOW + Back.BLUE + '-----------' + Back.RESET)
+    print('       ' + Fore.YELLOW + Back.BLUE + ' T | O | E ' + Back.RESET)
+    return ''
+
+
+def winner_message():
+    print(Fore.GREEN + '\n          Holy Smokes you won!')
+    print(Fore.GREEN + '       Check out your score below')
+    print(
+        Fore.GREEN + ' How many times do you think you can win?' + Fore.RESET
+        )
+    return ''
+
+
+def loser_message():
+    print(Fore.RED + '             Oh No! You lost!')
+    print(
+        Fore.RED +
+        ' Keep trying, im sure you will win next time' +
+        Fore.RESET
+        )
+    return ''
+
+
+def tie_message():
+    print(Fore.YELLOW + "        Well... It's a TIE!")
+    print(Fore.YELLOW + " You're both winners in my eyes <3" + Fore.RESET)
+    return ''
+
 
 
 def get_player_name():
